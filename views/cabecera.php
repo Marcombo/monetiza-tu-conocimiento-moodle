86045b1f205512516c762cd60ca115a460b278cb
TeverDiaz <71498832+TeverDiaz@users.noreply.github.com>
2022-04-05 15:21:08 +0200

    Add files via upload

---
+++ views/cabecera.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Academia Alma Mater</title>
+    <link href="https://fonts.googleapis.com/css?family=Actor" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/academia.css">
+    <script src="https://use.fontawesome.com/bc32f7bfed.js"></script>
+</head>
+
+<body>
+    <!-- Divisor de la Cabecera -->
+    <div id="cabecera">
+        <div id="logo">
+            <a href="index.php">
+                <img src="img/logo.png" alt="Academia Alma Mater">
+            </a>
+        </div>
+        <div id="menu">
+            <a href="index.php">Inicio</a>
+            <a href="gestionAlumnos.php">Gestión Alumnos</a>
+            <a href="gestionAsignaturas.php">Gestion Asignaturas</a>
+        </div>
+    </div>
---
+++ views/pieDePagina.php
@@ -0,0 +1,12 @@
+<!-- Divisor del Pie de Página -->
+<br><br>
+<div id="pieDePagina">
+    <div id="terminosLegales">
+        <a href="privacidad.html">Política de Privacidad</a>
+        <div id="redesSociales">
+            <a href="https://facebook.com"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
+            <a href="https://linkedin.com"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
+        </div>
+    </div>
+</div>
+</body>
